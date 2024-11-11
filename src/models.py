a25bf7437475d9ad5cff2683403435db09e973f2
hynduf <chauhuynhnoc@gmail.com>
2024-11-11 21:01:59 +0700

    11/11

--- src/models.py
+++ src/models.py
@@ -366,22 +366,23 @@ class Contrast(nn.Module):
 
         return self.lam * lori_a + (1 - self.lam) * lori_b, torch.cat((za_proj, zb_proj), 1)
 
+
 class EnsembleEmbedding(nn.Module):
     def __init__(self, embeddings, sizes = [100, 300], target_size = 128):
         super(EnsembleEmbedding, self).__init__()
+        num_dims = len(embeddings)
         self.params = nn.ParameterList([nn.Parameter(torch.from_numpy(embedding).float(), requires_grad = True) for embedding in embeddings])
-        
-        layers_dim = [sum(sizes), 512, 256, target_size]
-        self.linears = nn.ModuleList([nn.Sequential(
-            nn.Linear(layers_dim[i], layers_dim[i+1]),
-            nn.ReLU(),
-            nn.Dropout(0.2)
-        )for i in range(3)])
-
+        # self.linears = nn.ModuleList([nn.Linear(sizes[i], mid_size) for i in range(num_dims)])
+        self.linear = nn.Linear(sum(sizes), target_size)
+        self.relu = nn.ReLU()
     def forward(self):
+        # embeds = []
+        # for i, param in enumerate(self.params):
+        #     embed_prj = self.relu(self.linears[i](normalize(param)))
+        #     embeds.append(embed_prj)
+        # embeds = torch.cat(embeds, dim = -1)
         embeds = torch.cat([normalize(param.data) for param in self.params], dim=-1)
-        for linear in self.linears:
-            embeds = linear(embeds)
+        embeds = self.linear(embeds)
         return embeds
         
 
