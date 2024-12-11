41f801ab63ee2ae50b2b415c05d4e2d01d99b337
Nguyen Cong <congn1@ghtk.co>
2024-12-11 16:30:10 +0700

    KLTN 12/12/2024 KIBA

--- training.py
+++ training.py
@@ -110,14 +110,14 @@ if __name__ == '__main__':
     parser.add_argument('--gpus', type=str, default='0', help='Number of GPUs') # 0 -> CPU
     parser.add_argument('--cuda', type=int, default=0)
     parser.add_argument('--data_path', type=str, default='/kaggle/input/msgc-dta/MSGC-DTA/data/')
-    parser.add_argument('--dataset', type=str, default='davis')
+    parser.add_argument('--dataset', type=str, default='kiba')
     parser.add_argument('--epochs', type=int, default=6000)    # --kiba 3000
     parser.add_argument('--batch_size', type=int, default=512)
     parser.add_argument('--lr', type=float, default=0.0002)
-    parser.add_argument('--edge_dropout_rate', type=float, default=0.2)   # --kiba 0.
+    parser.add_argument('--edge_dropout_rate', type=float, default=0.)   # --kiba 0.
     parser.add_argument('--tau', type=float, default=0.8)
     parser.add_argument('--lam', type=float, default=0.5)
-    parser.add_argument('--num_pos', type=int, default=3)    # --kiba 10
+    parser.add_argument('--num_pos', type=int, default=10)    # --kiba 10
     parser.add_argument('--pos_threshold', type=float, default=8.0)
 
     args, _ = parser.parse_known_args()
